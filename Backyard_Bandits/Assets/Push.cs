369d573ac8e3588b799a85fdbe61764425a1c2cf
Melanie Werner <melaniecwerner@gmail.com>
2023-05-21 18:00:08 +0200

    add lights to prototype
    pushing works, pulling does not

--- Backyard_Bandits/Assets/Push.cs
+++ Backyard_Bandits/Assets/Push.cs
@@ -1,7 +1,69 @@
+using System;
+using TMPro;
 using UnityEngine;
 
 public class Push : MonoBehaviour
 {
+    
+    public KeyCode pushPullKey;
+    public float pushPullForce = 10f;
+    private bool isTouching = false;
+    private bool isPushingPulling = false;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // Check if the push/pull key is held down
+        if (false)
+        {
+            isPushingPulling = isTouching;
+        }
+        else
+        {
+            isPushingPulling = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply force to the object if the player is pushing/pulling and touching it
+        if (isPushingPulling && isTouching)
+        {
+            // Get the direction of the push/pull based on player's input
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 pushPullDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+
+            // Apply the force to the object's rigidbody
+            rb.AddForce(pushPullDirection * pushPullForce);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player is touching the object
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTouching = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Check if the player is no longer touching the object
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isTouching = false;
+        }
+    }
+    
+    
+    /*
     private Rigidbody _rigidbody;
 
     void Start()
@@ -26,5 +88,11 @@ public class Push : MonoBehaviour
             _rigidbody.AddForce(pushDirection * 10f, ForceMode.Impulse);
         }
     }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _rigidbody.velocity = Vector3.zero; 
+    }
+    */
 }
 
--- Backyard_Bandits/Assets/StarterAssets/ThirdPersonController/Scripts/BasicRigidBodyPush.cs
+++ Backyard_Bandits/Assets/StarterAssets/ThirdPersonController/Scripts/BasicRigidBodyPush.cs
@@ -1,16 +1,47 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Numerics;
+using StarterAssets;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+using UnityEngine.InputSystem;
+#endif
+
+[RequireComponent(typeof(CharacterController))]
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+[RequireComponent(typeof(PlayerInput))]
+#endif
 
 public class BasicRigidBodyPush : MonoBehaviour
 {
 	public LayerMask pushLayers;
 	public bool canPush;
 	[Range(0.5f, 5f)] public float strength = 1.1f;
+	
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+	private PlayerInput _playerInput;
+#endif
+	private CharacterController _controller;
+	private StarterAssetsInputs _input;
+	[SerializeField] GameObject _player;
+	private GameObject thingToPull;
+	private bool isPushingPulling = false;
+	private void Start()
+	{
+		_controller = GetComponent<CharacterController>();
+		_input = GetComponent<StarterAssetsInputs>();
+#if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+		_playerInput = GetComponent<PlayerInput>();
+#else
+			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
+#endif
+	}
 
 	private void OnControllerColliderHit(ControllerColliderHit hit)
 	{
 		if (canPush) PushRigidBodies(hit);
 	}
-
+	
 	private void PushRigidBodies(ControllerColliderHit hit)
 	{
 		// https://docs.unity3d.com/ScriptReference/CharacterController.OnControllerColliderHit.html
@@ -25,11 +56,36 @@ public class BasicRigidBodyPush : MonoBehaviour
 
 		// We dont want to push objects below us
 		if (hit.moveDirection.y < -0.3f) return;
-
+		
 		// Calculate push direction from move direction, horizontal motion only
-		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
-
+		//Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+		
+		/*if(_input.pull)
+		{
+			
+			thingToPull = body;
+			
+			Debug.Log("Pulling");
+			//Vector3 pullDir = new Vector3(_input.move.x, 0.0f);
+			Vector3 playerPosition = transform.position;
+			Vector3 objectPosition = body.transform.position;
+			Vector3 pullDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+			body.AddForce(-pullDir * strength, ForceMode.Impulse);
+			
+		}
+		*/
+		/*else
+		{
+			body.isKinematic = false;
+			Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z).normalized;
+			//body.transform.position += pushDir * Time.deltaTime * strength;
+			body.AddForce(pushDir * strength, ForceMode.Impulse);
+		}
+		*/
 		// Apply the push and take strength into account
+		body.isKinematic = false;
+		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z).normalized;
+		//body.transform.position += pushDir * Time.deltaTime * strength;
 		body.AddForce(pushDir * strength, ForceMode.Impulse);
 	}
 }
