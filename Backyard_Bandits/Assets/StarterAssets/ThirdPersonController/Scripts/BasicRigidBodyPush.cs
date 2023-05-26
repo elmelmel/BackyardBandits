6a9d9c5d92997c7314adb48dafb9a6ccee47fbb6
Melanie Werner <melaniecwerner@gmail.com>
2023-05-26 15:44:02 +0200

    pull
    sort of works now, will still have to weak some things

--- Backyard_Bandits/Assets/StarterAssets/ThirdPersonController/Scripts/BasicRigidBodyPush.cs
+++ Backyard_Bandits/Assets/StarterAssets/ThirdPersonController/Scripts/BasicRigidBodyPush.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Numerics;
 using StarterAssets;
+using UnityEditor.Searcher;
 using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
@@ -25,7 +27,7 @@ public class BasicRigidBodyPush : MonoBehaviour
 	private StarterAssetsInputs _input;
 	[SerializeField] GameObject _player;
 	private GameObject thingToPull;
-	private bool isPushingPulling = false;
+	private bool _pull = false;
 	private void Start()
 	{
 		_controller = GetComponent<CharacterController>();
@@ -36,10 +38,44 @@ public class BasicRigidBodyPush : MonoBehaviour
 			Debug.LogError( "Starter Assets package is missing dependencies. Please use Tools/Starter Assets/Reinstall Dependencies to fix it");
 #endif
 	}
-
+	
 	private void OnControllerColliderHit(ControllerColliderHit hit)
 	{
-		if (canPush) PushRigidBodies(hit);
+		if (canPush && !hit.gameObject.CompareTag("Pull")) PushRigidBodies(hit);
+	}
+
+	
+	private void OnTriggerStay(Collider other)
+	{
+		if (_input.pull && other.gameObject.CompareTag("Pull") && canPush)
+		{
+			Debug.Log("OnTriggerStay called");
+			_pull = true;
+			PullRigidBodies(other);
+		}
+	}
+	
+	private void OnTriggerExit(Collider other)
+	{
+		_pull = false;
+	}
+
+	private void PullRigidBodies(Collider hit)
+	{
+		Debug.Log("pulling");
+		Rigidbody body = hit.attachedRigidbody;
+		if (body == null || body.isKinematic || !_pull) return;
+
+		// make sure we only pull desired layer(s)
+		var bodyLayerMask = 1 << body.gameObject.layer;
+		if ((bodyLayerMask & pushLayers.value) == 0) return;
+
+		body.isKinematic = false;
+		BoxCollider bodyCollider = body.GetComponent<BoxCollider>();
+		//CapsuleCollider playerCollider = GetComponent<CapsuleCollider>();
+		Vector3 pullDir = new Vector3(transform.position.x - body.transform.position.x, 0.0f, 0.0f) - bodyCollider.size;
+		if(_input.move.y < 0) body.AddForce(pullDir * strength, ForceMode.Impulse);
+		//body.transform.parent = this.transform;
 	}
 	
 	private void PushRigidBodies(ControllerColliderHit hit)
@@ -57,35 +93,9 @@ public class BasicRigidBodyPush : MonoBehaviour
 		// We dont want to push objects below us
 		if (hit.moveDirection.y < -0.3f) return;
 		
-		// Calculate push direction from move direction, horizontal motion only
-		//Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
-		
-		/*if(_input.pull)
-		{
-			
-			thingToPull = body;
-			
-			Debug.Log("Pulling");
-			//Vector3 pullDir = new Vector3(_input.move.x, 0.0f);
-			Vector3 playerPosition = transform.position;
-			Vector3 objectPosition = body.transform.position;
-			Vector3 pullDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
-			body.AddForce(-pullDir * strength, ForceMode.Impulse);
-			
-		}
-		*/
-		/*else
-		{
-			body.isKinematic = false;
-			Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z).normalized;
-			//body.transform.position += pushDir * Time.deltaTime * strength;
-			body.AddForce(pushDir * strength, ForceMode.Impulse);
-		}
-		*/
 		// Apply the push and take strength into account
 		body.isKinematic = false;
 		Vector3 pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z).normalized;
-		//body.transform.position += pushDir * Time.deltaTime * strength;
 		body.AddForce(pushDir * strength, ForceMode.Impulse);
 	}
 }
